e68085e6b77961b744faf444427ac6866de1e23e
zpcdc6 <842101451@qq.com>
2016-04-08 10:45:51 +0800

    abc

--- CloudService/CloudService/青牛/ButelHandle.m
+++ CloudService/CloudService/青牛/ButelHandle.m
@@ -86,8 +86,7 @@ static ButelHandle *singleHandle = nil;
             
         }
     }else{
-        [MBProgressHUD showMessag:@"正在登陆青牛" toView:nil];
-    
+        [MBProgressHUD showMessag:@"正在登陆青牛" toView:nil];    
     }
     
 }
