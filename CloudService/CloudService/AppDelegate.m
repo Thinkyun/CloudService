138f5b04b6512dad8fe11dc9fc848981a47f3c65
zpcdc6 <842101451@qq.com>
2016-04-26 10:18:58 +0800

    abc

--- CloudService/CloudService/AppDelegate.m
+++ CloudService/CloudService/AppDelegate.m
@@ -111,7 +111,7 @@
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex NS_DEPRECATED_IOS(2_0, 9_0) {
     
     if(buttonIndex == 0) {
-        
+
         [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.ddyunf.com/html/download.html"]];
     }
 }
